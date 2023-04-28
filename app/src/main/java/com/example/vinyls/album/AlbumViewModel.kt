3c6c0eacb1cc2abc82d52a260edc0062499cf3bf
Milton Beltran <mj.beltran37@uniandes.edu.co>
2023-04-28 15:58:53 -0500

    Add HU01 with the new structure

--- app/src/main/java/com/example/vinyls/album/AlbumViewModel.kt
+++ app/src/main/java/com/example/vinyls/album/AlbumViewModel.kt
@@ -1,13 +1,56 @@
 package com.example.vinyls.album
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.vinyls.model.AlbumDBDao
+import com.example.vinyls.model.AlbumRepository
 
-class AlbumViewModel : ViewModel() {
+class AlbumViewModel(application: Application) :  AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is album Fragment"
+    private val _albums = MutableLiveData<List<AlbumDBDao>>()
+
+    val albums: LiveData<List<AlbumDBDao>>
+        get() = _albums
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+    private val albumRepository = AlbumRepository(application)
+    init {
+        refreshDataFromNetwork()
     }
-    val text: LiveData<String> = _text
+
+    private fun refreshDataFromNetwork() {
+        albumRepository.refreshData({
+            _albums.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(AlbumViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return AlbumViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
