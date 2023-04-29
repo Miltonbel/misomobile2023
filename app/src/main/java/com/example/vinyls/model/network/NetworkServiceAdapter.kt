895d915bc2c77c48fbd32930534e4ab2cb295353
Luis <luisfernandosantaospina@gmail.com>
2023-04-29 13:04:20 -0500

    Add get detail to network

--- app/src/main/java/com/example/vinyls/model/network/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/vinyls/model/network/NetworkServiceAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.vinyls.model.network
 
 import android.content.Context
+import android.util.Log
 import org.json.JSONArray
 import org.json.JSONObject
 import com.android.volley.Request
@@ -11,10 +12,11 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.vinyls.model.AlbumDBDao
+import com.example.vinyls.model.AlbumDetail
 
 class NetworkServiceAdapter constructor(context: Context) {
     companion object{
-        const val BASE_URL= "https://vynils-back-heroku.herokuapp.com/"
+        const val BASE_URL= "http://34.71.211.68/"
         var instance: NetworkServiceAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
@@ -36,6 +38,24 @@ class NetworkServiceAdapter constructor(context: Context) {
                     val item = resp.getJSONObject(i)
                     list.add(i, AlbumDBDao(albumId = item.getInt("id"),name = item.getString("name"), cover = item.getString("cover"), recordLabel = item.getString("recordLabel"), releaseDate = item.getString("releaseDate"), genre = item.getString("genre"), description = item.getString("description")))
                 }
+                onComplete(list)
+            },
+            Response.ErrorListener {
+                onError(it)
+            }))
+    }
+
+    fun getAlbumDetail(albumId:Int, onComplete:(resp:List<AlbumDetail>)->Unit, onError: (error:VolleyError)->Unit){
+        requestQueue.add(getRequest("albums/$albumId",
+            Response.Listener<String> { response ->
+                val resp = JSONObject(response)
+                val list = mutableListOf<AlbumDetail>()
+                var item:JSONObject? = null
+
+                item = resp
+                Log.d("Response", item.toString())
+                list.add(0, AlbumDetail(albumId = item.getInt("id"),name = item.getString("name"), cover = item.getString("cover"), recordLabel = item.getString("recordLabel"), releaseDate = item.getString("releaseDate"), genre = item.getString("genre"), description = item.getString("description")))
+
                 onComplete(list)
             },
             Response.ErrorListener {
