bda0e6cb889a606ab284e21b2efe56d3fc965355
Luis <luisfernandosantaospina@gmail.com>
2023-05-04 07:02:45 -0500

    Add model artist

---
+++ app/src/main/java/com/example/vinyls/model/Artist.kt
@@ -0,0 +1,9 @@
+package com.example.vinyls.model
+
+data class Artist (
+    val artistId:Int,
+    val name:String,
+    val image:String,
+    val description:String,
+    val creationDate:String
+)
